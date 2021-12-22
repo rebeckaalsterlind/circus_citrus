500c749de9d8cc57fdff6b381611c9c816ee86cf
EmmaHammar <emma.s.hammar@gmail.com>
2021-12-22 14:08:31 +0100

    adding animation of menu icon when opened and closed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,60 @@
-
-
-//menu
+let colorWhite = '#FFFFFF';
+let colorMenuText = '#D02060';
+//open menu
 document.getElementById("menu").addEventListener('click', () => {
   document.getElementById("sidenav").style.width = "1008px";
+
+  gsap.to('#menu-icon', {
+    x: -598,
+    y: -20,
+  })
+
+  gsap.to('#menu-icon-text', {
+    fill: colorMenuText,
+    rotate: 45,
+    x: 10,
+    y: -70,
+  
+  });
+  gsap.to('#menu-cross-left', {
+    stroke: colorMenuText,
+    rotate: -45,
+    y: 36,
+    x: -26,
+  });
+
+  //make one stroke of the cross invisible
+  gsap.to('#menu-cross-right', {
+    stroke: "none",
+  });
 });
 
-
+//close menu
 document.getElementById("close").addEventListener("click", () => {
   document.getElementById("sidenav").style.width = "0";
+
+  gsap.to('#menu-icon', {
+    x: 0,
+    y: 0,
+  })
+
+  gsap.to('#menu-icon-text', {
+    fill: colorWhite,
+    rotate: 0,
+    x: 0,
+    y: 0,
+  
+  });
+  gsap.to('#menu-cross-left', {
+    stroke: colorWhite,
+    rotate: 0,
+    y: 0,
+    x: 0,
+  });
+
+  gsap.to('#menu-cross-right', {
+    stroke: colorWhite,
+  });
 })
 
 // blob
